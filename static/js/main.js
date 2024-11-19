4bc4a07c98efcc8bb9d7f849ae7ba09619f6c022
achmadpasha <nabilahmadpasha@gmail.com>
2024-11-19 16:25:28 +0800

    create home section

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,43 @@ const menuItem = document.querySelector('#menu-item')
 menuToggle.addEventListener('click', function() {
   menuItem.classList.toggle('show')
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const autoType = document.getElementById("autotype");
+  const textArray = ["Tech Enthusias", "Programmer", "Junior Web Developer"];
+
+  let pipe = true;
+  let index = 0;
+  let speed = 75;
+  let isTyping = true;
+  let textIndex = 0;
+
+  function typeText() {
+    let text = textArray[textIndex];
+    if (pipe) {
+      text += "|";
+    }
+
+    if (isTyping) {
+      if (index < text.length) {
+        autoType.value += text[index];
+        index++;
+      } else {
+        isTyping = false;
+      }
+    } else {
+      if (index > 0) {
+        autoType.value = text.slice(0, index - 1);
+        index--;
+      } else {
+        isTyping = true;
+        textIndex = (textIndex + 1) % textArray.length;
+      }
+    }
+
+    setTimeout(typeText, speed);
+  }
+
+  typeText();
+});
